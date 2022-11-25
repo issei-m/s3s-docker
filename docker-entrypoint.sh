003fd32d334f9a07190912fdd88534cd72e29b96
Issei.M <issei.m7@gmail.com>
2022-11-26 00:44:05 +0900

    Update docker-entrypoint.sh
    complying with Shellcheck plugin:
    https://plugins.jetbrains.com/plugin/10195-shellcheck

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 config_file=/opt/s3s/config.txt
 
 if [ -f $config_file ]; then
-  printf "Configuration file $config_file has been mounted, it will be loaded\n"
+  printf "Configuration file %s has been mounted, it will be loaded\n" $config_file
 else
-  if [ ! -z "${S3S_CONFIG}" ]; then
-    printf "S3S_CONFIG environment variable has been specified, it will be written to $config_file\n"
-    echo "${S3S_CONFIG}" > $config_file
+  if [ -n "$S3S_CONFIG" ]; then
+    printf "S3S_CONFIG environment variable has been specified, it will be written to %s\n" $config_file
+    echo "$S3S_CONFIG" > $config_file
   else
     printf "[!] The s3s configuration is missing, so the program will interact you to take the credential.\n"
   fi
